ea662ef942ad88da895da8db4f05e8828f386532
Sabo Ofc <107702229+SaboOfficial@users.noreply.github.com>
2022-07-01 01:56:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ global.owner = [
   ['628815952469', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = ['6282145203493','628815952469'] // Want some help?
+global.prems = ['6282145203493','628815952469'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
